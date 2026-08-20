31963e89e6c94f02adac295825ba3d905e819ac0
Arzy <fatih.arrazy@gmail.com>
2026-08-20 17:03:00 +1000

    first commit

---
+++ js/level1/index.js
@@ -0,0 +1,17 @@
+let name = 'Mode'; // let is a way to declare dynamic vars
+const name = 'John'; // const is a way for a constant that cannot change
+
+
+console.log(name); 
+
+//make object here
+let person = {
+    name: 'Mode',
+    age: 20,
+    isApproved: true
+}
+
+person.name = 'John';
+
+
+console.log(person);
