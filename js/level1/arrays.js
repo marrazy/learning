04a56746e3f90c3793185dc783b0c783107ffa8c
Arzy <fatih.arrazy@gmail.com>
2026-08-20 17:31:07 +1000

    simple js

---
+++ js/level1/arrays.js
@@ -0,0 +1,6 @@
+
+let selectedColors = ['red', 'blue']; //array
+
+selectedColors[selectedColors.length] = 'green'; //add green to the end of the array
+
+console.log(selectedColors)
--- js/level1/index.js
+++ js/level1/index.js
@@ -2,16 +2,19 @@ let name = 'Mode'; // let is a way to declare dynamic vars
 const name = 'John'; // const is a way for a constant that cannot change
 
 
-console.log(name); 
+console.log(name); //print
 
-//make object here
+//make object here (its like a hashmap i think)
 let person = {
     name: 'Mode',
     age: 20,
     isApproved: true
 }
 
-person.name = 'John';
+let selection = 'name'; //this is a dynamic way to access object properties
 
+person.name = 'John'; //change object.name to John
+
+person[selection] = 'Moug'; //change object.age to 21
 
 console.log(person);
